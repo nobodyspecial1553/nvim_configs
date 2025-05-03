38cf823ac794b1f015fa6f0e2adbedb5ced6a08d
nobodyspecial1553 <degenkami@proton.me>
2025-05-02 19:10:29 -0500

    Added Tab Controls

--- lua/ns/remap.lua
+++ lua/ns/remap.lua
@@ -8,3 +8,7 @@ vim.keymap.set('n', '<C-k>', '<C-w>k') -- Up
 vim.keymap.set('n', '<C-j>', '<C-w>j') -- Down
 vim.keymap.set('n', '<C-h>', '<C-w>h') -- Left
 vim.keymap.set('n', '<C-l>', '<C-w>l') -- Right
+
+-- Tabs
+vim.keymap.set('n', '<Tab>', function() vim.cmd [[tabnext]] end )
+vim.keymap.set('n', '<S-Tab>', function() vim.cmd [[tabprevious]] end )
